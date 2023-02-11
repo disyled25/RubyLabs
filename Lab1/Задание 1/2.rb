2a96466a86c337c0c8d136fc714f6c3e4603fafb
disyled25 <nazarow.george@gmail.com>
2023-02-11 13:24:50 +0300

    Create 2.rb

---
+++ Lab1/Задание 1/2.rb
@@ -0,0 +1,14 @@
+user_name = ARGV[0]
+puts "Приветствую #{user_name}"
+puts "Какой язык программирования ваш любимый?"
+
+case STDIN.gets.chomp
+when "Ruby"
+	puts "Подлиза!"
+when "C++"
+	puts "Тебе больше 30"
+when "Python"
+	puts "Зумер"
+when "R"
+	puts "Любишь БОЛЬШИЕ данные?"
+else puts "Скоро будет Ruby!"
