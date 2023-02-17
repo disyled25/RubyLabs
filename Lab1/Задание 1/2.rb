8f02ac71c94aec3cf7bf6ea0faf4935b94f3f390
disyled25 <98869569+disyled25@users.noreply.github.com>
2023-02-17 22:58:27 +0300

    Update 2.rb

--- Lab1/Задание 1/2.rb
+++ Lab1/Задание 1/2.rb
@@ -3,13 +3,22 @@ puts "Приветствую #{user_name}"
 puts "Какой язык программирования ваш любимый?"
 
 case STDIN.gets.chomp
-  when "Ruby"
-	  puts "Подлиза!"
-  when "C++"
-	  puts "Тебе больше 30"
-  when "Python"
-	  puts "Зумер"
-  when "R"
-	  puts "Любишь БОЛЬШИЕ данные?"
-  else puts "Скоро будет Ruby!"
+	when "Ruby"
+		puts "Подлиза!"
+	when "C++"
+		puts "Тебе больше 30"
+	when "Python"
+		puts "Зумер"
+	when "R"
+		puts "Любишь БОЛЬШИЕ данные?"
+	else puts "Скоро будет Ruby!"
 end
+
+puts "Введите команду Ruby"
+s = STDIN.gets.chomp
+puts eval(s)
+
+puts "Введите команду Linux"
+s = STDIN.gets.chomp
+com = `#{s}`
+puts com
